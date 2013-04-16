5db09e525c60bb6b3e80080a2160a3312e456330
OpenShift guest <ishanjetley@gmail.com>
2013-04-15 23:37:42 -0400

    Setting Up Git Hub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'ecb0d4641158017a67d865cfa4ee01bd31d9b507e216a527ad860acf858f934058afac55e0d5c0456f23641fb21fcda7b96cf657232e46121951af82842bfa1b'
