a4e17123023dae00b0f2dc31b7e907e4f249fe1e
OpenShift guest <ishanjetley@gmail.com>
2013-04-27 21:41:33 -0400

    Added relationships, names,

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name , :business_name , :business_category
   # attr_accessible :title, :body
+  
+  has_many :statuses
+  
+  def full_name 
+      first_name + " " + last_name       
+  end
+  
+  
 end
